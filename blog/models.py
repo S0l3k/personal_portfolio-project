fc3f357358fd16fdec3705f21913e67ad27e061f
S0l3k <turtleteamwcg@gmail.com>
2023-05-03 21:07:20 +0300

    edit: models blog and portfolio, now admin panel show title projects

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=200)
     date = models.DateField()
+
+    def __str__(self):
+        return self.title
